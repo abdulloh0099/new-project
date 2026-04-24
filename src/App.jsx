527ef036bda8a935099134471bae6d4589d9072e
abdulloh0099 <abdulhamidov118@gmail.com>
2026-04-24 21:20:41 +0500

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import rasm from './assets/rasm.png';
 import rasm1 from './assets/rasm1.png';
 import rasm2 from './assets/rasm2.png';
 import rasm3 from './assets/rasm3.png';
+import rasm4 from './assets/rasm4.png';
+import rasm5 from './assets/rasm5.png';
+import rasm6 from './assets/rasm6.png';
+import rasm7 from './assets/rasm7.png';
+import mountain from './assets/mountain.png';
 import Card from './Card';
 const App = () => {
   return (
@@ -35,6 +40,74 @@ const App = () => {
           department="Marketing"
           rasm={rasm3}
         />
+        <Card
+          name2="Takumi Sato"
+          position2="HR Specialist"
+          department2="Human Resources"
+          rasm2={rasm4}
+        />
+        <Card
+          name2="Chen Wei"
+          position2="Data Analyst"
+          department2="Data science"
+          rasm2={rasm5}
+        />
+        <Card
+          name2="Emma Dubois"
+          position2="Sales Manager"
+          department2="Sales"
+          rasm2={rasm6}
+        />
+        <Card
+          name2="Igor Sokolov"
+          position2="Quality Assurence"
+          department2="Quality Control"
+          rasm2={rasm7}
+        />
+      </div>
+      <h1>Info Card</h1>
+      <div className="card-i">
+      <div className="card-1">
+        <img src={mountain} alt="" className="card-1-img" />
+          <h1 className="card-1-h1">Unraveling the Mysteries of the Ocean</h1>
+        <p className="card-1-p">
+          From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration.
+        </p>
+        <button className="card-1-btn">Read More</button>
+      </div>
+      <div className="card-1">
+        <img src={mountain} alt="" className="card-1-img" />
+          <h1 className="card-1-h1">Unraveling the Mysteries of the Ocean</h1>
+        <p className="card-1-p">
+          From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration.
+        </p>
+        <button className="card-1-btn">Read More</button>
+      </div>
+      <div className="card-1">
+        <img src={mountain} alt="" className="card-1-img" />
+          <h1 className="card-1-h1">Unraveling the Mysteries of the Ocean</h1>
+        <p className="card-1-p">
+          From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration.
+        </p>
+        <button className="card-1-btn">Read More</button>
+      </div>
+      <div className="card-1">
+        <img src={mountain} alt="" className="card-1-img" />
+          <h1 className="card-1-h1">Unraveling the Mysteries of the Ocean</h1>
+        <p className="card-1-p">
+          From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration.
+        </p>
+        <button className="card-1-btn">Read More</button>
+      </div>
+      <div className="card-1">
+        <img src={mountain} alt="" className="card-1-img" />
+          <h1 className="card-1-h1">Unraveling the Mysteries of the Ocean</h1>
+        <p className="card-1-p">
+          From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration.
+        </p>
+        <button className="card-1-btn">Read More</button>
+      </div>
+
       </div>
     </div>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import './Card.css'
-const Card = (props) => {
+import React from 'react';
+import './Card.css';
+const Card = props => {
   return (
     <div>
-     
-        <div className="card">
-          <img src={props.rasm} alt="" className="card-img" />
-          <h1 className='card-h1'>{props.name}</h1>
-          <p className='card-p'>{props.position}</p>
-          <p className='card-p'>{props.department}</p>
-        </div>
+
+      <div className={props.name2 ? 'card2' : 'card'}>
+        <img src={props.rasm || props.rasm2} className="card-img" />
+        <h1 className="card-h1">{props.name || props.name2}</h1>
+        <p className="card-p">{props.position || props.position2}</p>
+        <p className="card-p">{props.department || props.department2}</p>
       </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Card
+export default Card;
